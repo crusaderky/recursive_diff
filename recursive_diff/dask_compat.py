9dc81bc936bd5480c1053e0ff02321df1b679176
Guido Imperiale <crusaderky@gmail.com>
2026-02-09 20:42:31 +0000

    Compute Dask objects later (#48)

--- recursive_diff/dask_compat.py
+++ recursive_diff/dask_compat.py
@@ -4,13 +4,21 @@ try:
     from dask import compute
     from dask.delayed import Delayed
 
-    def is_delayed(x: object) -> bool:
+    def is_dask_delayed(x: object) -> bool:
         return isinstance(x, Delayed)
+
+    def is_dask_collection(x: object) -> bool:
+        f = getattr(x, "__dask_keys__", None)
+        return bool(f()) if f is not None else False
+
 
 except ImportError:
 
     def compute(*args: object) -> object:  # type: ignore[misc]
         return args
 
-    def is_delayed(_: object) -> bool:  # type: ignore[misc]
+    def is_dask_delayed(_: object) -> bool:  # type: ignore[misc]
+        return False
+
+    def is_dask_collection(_: object) -> bool:  # type: ignore[misc]
         return False
