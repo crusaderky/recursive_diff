9f5bf549238ddaccd60b18614243677096b1355d
crusaderky <crusaderky@gmail.com>
2026-02-18 22:10:10 +0000

    Expose brief_dims to recursive_eq; fix brief_dims bug

--- recursive_diff/tests/test_recursive_eq.py
+++ recursive_diff/tests/test_recursive_eq.py
@@ -1,4 +1,5 @@
 import pytest
+import xarray
 
 from recursive_diff import recursive_eq
 
@@ -10,9 +11,27 @@ def test_recursive_eq_success(capsys):
 
 def test_recursive_eq_fail(capsys):
     # Test the actual log lines dumped out by recursive_eq
-    with pytest.raises(AssertionError):
+    with pytest.raises(AssertionError, match="Found 2 differences"):
         recursive_eq(("foo", ("bar", "baz")), ("foo", ("bar", "asd", "lol")))
     assert capsys.readouterr().out.splitlines() == [
         "[1]: RHS has 1 more elements than LHS: ['lol']",
         "[1][1]: baz != asd",
+    ]
+
+
+def test_recursive_eq_brief_dims(capsys):
+    a = xarray.DataArray([[1, 2, 3], [4, 5, 6]], dims=["x", "y"])
+    b = xarray.DataArray([[1, 3, 5], [4, 5, 7]], dims=["x", "y"])
+    with pytest.raises(AssertionError, match="Found 3 differences"):
+        recursive_eq(a, b)
+    assert capsys.readouterr().out.splitlines() == [
+        "[data][x=0, y=1]: 2 != 3 (abs: 1.0e+00, rel: 5.0e-01)",
+        "[data][x=0, y=2]: 3 != 5 (abs: 2.0e+00, rel: 6.7e-01)",
+        "[data][x=1, y=2]: 6 != 7 (abs: 1.0e+00, rel: 1.7e-01)",
+    ]
+    with pytest.raises(AssertionError, match="Found differences"):
+        recursive_eq(a, b, brief_dims=["x"])
+    assert capsys.readouterr().out.splitlines() == [
+        "[data][y=1]: 1 differences",
+        "[data][y=2]: 2 differences",
     ]
